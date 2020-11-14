35e8fa715c9672903752d659efd4f389f779e99d
Phung Nguyen <phungnnl@dgroup.co>
2020-11-14 12:13:35 +0700

    WIP update back-end

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SplittersModuleModule } from './splitters-module/splitters-module.module';
+import { SplittersModule } from './splitters/splitters.module';
+import { SplitterService } from './splitter/splitter.service';
 
 @Module({
-  imports: [],
+  imports: [SplittersModuleModule, SplittersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SplitterService],
 })
 export class AppModule {}
